b871dea34bc18adb6775d3b764148991ce59609b
Qasim Malik <114612340+qasim0786@users.noreply.github.com>
2024-11-17 19:18:50 +0500

    latest-01

--- 45package-json/index.js
+++ 45package-json/index.js
@@ -1,8 +1,7 @@
 var colors = require('colors');
  
-console.log('hello'.green); // outputs green te
-console.log('hello'.red); // outputs green te
-console.log('hello'.rainbow); // outputs green te
-console.log('hello'.rainbow); // outputs green te
-
-console.log('hello'.yellow); // outputs green te
+console.log('hello'.green); // outputs green text
+console.log('i like cake and pies'.underline.red) // outputs red underlined text
+console.log('inverse the color'.inverse); // inverses the color
+console.log('OMG Rainbows!'.rainbow); // rainbow
+console.log('Run the trap'.trap); // Drops the ba
