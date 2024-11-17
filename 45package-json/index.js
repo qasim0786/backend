7e1be425e9812dfaf4cc5dce7ee0c231748ae6d1
Qasim Malik <114612340+qasim0786@users.noreply.github.com>
2024-11-17 19:12:26 +0500

    new-commit

---
+++ 45package-json/index.js
@@ -0,0 +1,8 @@
+var colors = require('colors');
+ 
+console.log('hello'.green); // outputs green te
+console.log('hello'.red); // outputs green te
+console.log('hello'.rainbow); // outputs green te
+console.log('hello'.rainbow); // outputs green te
+
+console.log('hello'.yellow); // outputs green te
